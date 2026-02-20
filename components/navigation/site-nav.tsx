3066e031489fc4571a228052b28b3e179d0cb70a
bighog300 <143169285+bighog300@users.noreply.github.com>
2026-02-20 15:18:59 +0000

    Remove unused legacy site nav components

--- components/navigation/site-nav.tsx
+++
@@ -1,7 +0,0 @@
-import { getSessionUser } from "@/lib/auth";
-import { SiteNavClient } from "@/components/navigation/site-nav-client";
-
-export async function SiteNav() {
-  const user = await getSessionUser();
-  return <SiteNavClient isAuthenticated={Boolean(user)} />;
-}
